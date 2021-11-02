69f196021a9e092558e77bf5ef8acead4fffe601
Kyle <47279470+unblinky@users.noreply.github.com>
2021-11-01 19:58:50 -0700

    Added Bullet logic. Needs art and sound.

---
+++ Code/Centerpede/Scenes/Bullet.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+export var speed = 600.0 # 300 - 1200
+
+func _process(delta_time):	
+	if position.y < 50:
+		Kill()
+		pass
+	position.y -= speed * delta_time
+	
+
+func Kill():
+	queue_free()
