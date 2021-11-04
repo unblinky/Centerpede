6e06766de2388c4f7a7c669e337d4a0081a11a40
Kyle <47279470+unblinky@users.noreply.github.com>
2021-11-04 14:29:24 -0700

    Exporting animated sprite to Godot.

--- Code/Centerpede/Scenes/Main.gd
+++ Code/Centerpede/Scenes/Main.gd
@@ -2,4 +2,3 @@ extends Node
 
 func _ready():
 	print("I am a mighty CENTERPEDE!")
-
--- Code/Centerpede/Scenes/Player.gd
+++ Code/Centerpede/Scenes/Player.gd
@@ -8,36 +8,33 @@ var direction: Vector2
 var velocity: Vector2
 var ammo_count: int = 1
 
-func _process(delta_time):
-	# Input Direction
-	var x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	var y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-	direction = Vector2(x, y).normalized()
-
-	# Input Fire
+func _process(_delta_time):
+	# Input Direction.
+	var x: float = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var y: float = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	direction = Vector2(x, y)
+	
+	if direction.length() > 1:
+		direction = direction.normalized()
+		
+	#position += direction * speed * delta_time
+	#velocity = direction * speed * delta_time
+	# warning-ignore:return_value_discarded
+	move_and_slide(direction * speed)
+	
+	# Input Fire.
 	if Input.is_action_pressed("fire"):
 		Fire()
-
-	Move(direction * speed * delta_time)
-	
-
-func Move(delta_position: Vector2):
-	position += delta_position
-
-
-func Reload(count: int):
-	ammo_count += count
-	print("Ammo Count: " + str(ammo_count))
-	
 	
 func Fire():
 	if ammo_count > 0:
-		# Spawn bullet
 		var bullet: Bullet = BULLET_PS.instance()
 		bullet.player = self
 		bullet.position = position
-		get_parent().get_node("Bullets").add_child(bullet)
-		print("Fire!")
-
+		get_node("../../Bullets").add_child(bullet)
 		ammo_count -= 1
+		print("Fire!")
 	
+func Reload(count: int):
+	ammo_count += count
+	print("Ammo Count: " + str(ammo_count))
