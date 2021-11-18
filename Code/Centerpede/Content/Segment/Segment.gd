068edf4cce5f9311a8859a6553c35e1dfb107225
Kyle <47279470+unblinky@users.noreply.github.com>
2021-11-17 20:13:12 -0800

    Setup the Segment class. But it has a bug with queue_free().

--- Code/Centerpede/Content/Segment/Segment.gd
+++ Code/Centerpede/Content/Segment/Segment.gd
@@ -1,3 +1,24 @@
 extends Node2D
 
+const MUSHROOM_PS: PackedScene = preload("res://Content/Mushroom/Mushroom.tscn")
 
+export var speed: float = 60 # Pixels / sec.
+var direction: Vector2 = Vector2.LEFT
+var velocity: Vector2
+
+
+func _process(delta_time):
+	position += speed * direction * delta_time
+	
+	
+func _on_Segment_body_entered(body):
+	if body is Bullet:
+		body.Kill()
+		
+		var shroom = MUSHROOM_PS.instance()
+		shroom.position = position
+		#get_node("../").add_child(shroom)
+		#call_deferred("Kill")
+		
+func Kill():
+	queue_free()
