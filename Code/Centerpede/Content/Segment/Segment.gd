32838b22f287e79c2a1119271962a9cc4cdc4e42
Kyle <47279470+unblinky@users.noreply.github.com>
2021-11-22 18:12:44 -0800

    Revert "Clues"
    This reverts commit 6357ffdd2645e6620f1c9e3bb196404a71f49b4f.

--- Code/Centerpede/Content/Segment/Segment.gd
+++ Code/Centerpede/Content/Segment/Segment.gd
@@ -1,23 +1,24 @@
 extends Node2D
 
-var MAIN: Node
 const MUSHROOM_PS: PackedScene = preload("res://Content/Mushroom/Mushroom.tscn")
 
 export var speed: float = 60 # Pixels / sec.
 var direction: Vector2 = Vector2.LEFT
 var velocity: Vector2
 
+
 func _process(delta_time):
 	position += speed * direction * delta_time
-
-func Init(my_main: Node):
-	MAIN = my_main
-	return self
+	
 	
 func _on_Segment_body_entered(body):
 	if body is Bullet:
+		body.Kill()
+		
 		var shroom = MUSHROOM_PS.instance()
 		shroom.position = position
-		MAIN.call_deferred("add_child", shroom)
-		#MAIN.SpawnMushroom(position)
+		#get_node("../").add_child(shroom)
+		#call_deferred("Kill")
 		
+func Kill():
+	queue_free()
