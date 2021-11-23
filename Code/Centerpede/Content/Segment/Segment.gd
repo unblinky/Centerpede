3b6e5bf389806bebd2cdc6c55d088304189cc28e
Kyle <47279470+unblinky@users.noreply.github.com>
2021-11-22 19:35:43 -0800

    Segment flips x direction upon hitting a Mushroom.

--- Code/Centerpede/Content/Segment/Segment.gd
+++ Code/Centerpede/Content/Segment/Segment.gd
@@ -6,6 +6,7 @@ export var speed: float = 60 # Pixels / sec.
 var direction: Vector2 = Vector2.LEFT
 var velocity: Vector2
 
+var is_headed_left: bool = true # TODO: Randomize at spawn?
 var is_dropping: bool
 var down_pos: Vector2
 
@@ -13,8 +14,12 @@ func _process(delta_time):
 	position += speed * direction * delta_time
 	if is_dropping:
 		if position.distance_to(down_pos) > 16:
-			direction = Vector2.RIGHT
+			is_headed_left = !is_headed_left
 			is_dropping = false
+			if is_headed_left:
+				direction = Vector2.LEFT
+			else:
+				direction = Vector2.RIGHT
 	
 	
 func _on_Segment_body_entered(body):
